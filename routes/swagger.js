1d74e992bc264ce1587f2526ab13aed94d96c47f
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2019-07-27 11:12:44 -0500

    redux added

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerDocument = require('../lib/swagger/swagger.json');
 
 router.use('/', swaggerUi.serve);
 router.get('/', swaggerUi.setup(swaggerDocument));
-
+router.get('/editor',function(req, res, next) {
+    res.send({"data":"api fro editor"});
+  });
 
 module.exports = router;
