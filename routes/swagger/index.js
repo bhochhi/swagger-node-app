48bc1f8946e0d153da09a228d5ed7f9bbca656de
Rupesh Bhochhibhoya <bhochhi@gmail.com>
2019-07-27 03:05:00 -0500

    create express server and react for client

--- routes/swagger/index.js
+++ routes/swagger/index.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerEditor = require('swagger-editor-dist');
 const swaggerDocument = require('./swagger.json');
 
 router.use('/', swaggerUi.serve);
 router.get('/', swaggerUi.setup(swaggerDocument));
-router.get('/editor', swaggerEditor);
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('swagger resource');
-// });
 
 module.exports = router;
